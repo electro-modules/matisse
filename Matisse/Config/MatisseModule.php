48f6f212fffbfa3de3ad658a502da96deb40a480
Cláudio Silva <claudio.silva@impactwave.com>
2016-11-03 19:19:04 +0000

    Module configuration refactoring.

--- Matisse/Config/MatisseModule.php
+++ Matisse/Config/MatisseModule.php
@@ -2,31 +2,31 @@
 namespace Electro\Plugins\Matisse\Config;
 
 use Electro\Interfaces\DI\InjectorInterface;
+use Electro\Interfaces\KernelInterface;
 use Electro\Interfaces\ModuleInterface;
 use Electro\Interfaces\Views\ViewServiceInterface;
 use Electro\Kernel\Lib\ModuleInfo;
-use Electro\Kernel\Services\Bootstrapper;
 use Electro\Plugins\Matisse\Interfaces\DataBinderInterface;
 use Electro\Plugins\Matisse\Lib\DataBinder;
 use Electro\Plugins\Matisse\Lib\MatisseEngine;
 use Electro\Plugins\Matisse\Parser\DocumentContext;
 use Electro\Plugins\Matisse\Services\MacrosService;
-use const Electro\Kernel\Services\REGISTER_SERVICES;
 
 class MatisseModule implements ModuleInterface
 {
-  static function bootUp (Bootstrapper $bootstrapper, ModuleInfo $moduleInfo)
+  static function startUp (KernelInterface $kernel, ModuleInfo $moduleInfo)
   {
-    $bootstrapper->on (REGISTER_SERVICES, function (InjectorInterface $injector) {
-      $injector
-        ->share (DocumentContext::class)
-        ->share (MacrosService::class)
-        ->share (MatisseSettings::class)
-        ->alias (DataBinderInterface::class, DataBinder::class)
-        ->prepare (ViewServiceInterface::class, function (ViewServiceInterface $viewService) {
-          $viewService->register (MatisseEngine::class, '/\.html$/');
-        });
-    });
+    $kernel->onRegisterServices (
+      function (InjectorInterface $injector) {
+        $injector
+          ->share (DocumentContext::class)
+          ->share (MacrosService::class)
+          ->share (MatisseSettings::class)
+          ->alias (DataBinderInterface::class, DataBinder::class)
+          ->prepare (ViewServiceInterface::class, function (ViewServiceInterface $viewService) {
+            $viewService->register (MatisseEngine::class, '/\.html$/');
+          });
+      });
   }
 
 }
