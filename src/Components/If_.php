40e796857f43392d723f27cba8a334a611d6d783
Cláudio Silva <claudio.silva@impactwave.com>
2016-12-23 02:18:09 +0000

    FIX: <IF {variable}> (instead of <If {variable!=null}>) not working. FIX: other IF issues.

--- src/Components/If_.php
+++ src/Components/If_.php
@@ -17,13 +17,15 @@ class IfProperties extends ComponentProperties
    */
   public $else = type::content;
   /**
-   * @var string A regular expression.
+   * A regular expression. Ex: `'/some .* apples/i'`
+   *
+   * @var string
    */
   public $matches = '';
   /**
    * @var bool Assigned via the `<If {exp}>` syntax.
    */
-  public $nameless = type::bool;
+  public $nameless = type::any;
   /**
    * > **Mote:** it doesn't work with databinding.
    *
@@ -37,28 +39,50 @@ class IfProperties extends ComponentProperties
 }
 
 /**
- * Rendes content blocks conditionally.
+ * Renders content blocks conditionally.
  *
- * ##### Syntax:
+ * ##### Basic syntax:
  * ```
  * <If {exp}>
  *   content if truthy (not null nor an empty string)
- *   <Else> content if falsy </Else>
+ *   <Else>
+ *     content if falsy (note: the Else block is optional)
+ *   </Else>
+ * </If>
+ * ```
+ * <p>`{exp}` is a databinding expression. Ex: `{some.prop == 5}`
+ * <p>Note that when `{exp}` is a string value, it's considered `false` when its value is `'false'`, `'no'`, `'off'`,
+ * `'0'` or `''`.
+ *
+ * ##### Other forms:
+ * ```
+ * <If value={exp}>                   This is the same as <If {exp}>
+ *   content if exp is truthy         You can use either on the examples below.
  * </If>
  *
- * <If value="value"> content if value is truthy </If>    // this is the same as <If {value}>
+ * <If not {exp}>                     This is the same as <If {!exp}> or <If not value={exp}>
+ *   content if exp is falsy
+ * </If>
  *
- * <If not value="value"> content if value is falsy </If> // this is the same as <If {!value}>
+ * <If {exp} matches="regexp">        This is the same as <If value={exp} matches="regexp">
+ *   content if exp matches the regular expression
+ * </If>
  *
- * <If value="value" matches="regexp"> content if value matches the regular expression </If>
+ * <If {exp} not matches="regexp">    This is the same as <If value={exp} not matches="regexp">
+ *   content if exp doesn't match the regular expression
+ * </If>
  *
- * <If value="value" not matches="regexp"> content if value doesn't match the regular expression </If>
- *
- * <If value="value">
- *   <Case is="value1"> content if value == value1 </Case>
+ * <If {exp}>
+ *   <Case is=value1>
+ *     content if exp == value1
+ *   </Case>
  *   ...
- *   <Case is="valueN"> content if value == valueN </Case>
- *   <Else> content if no match </Else>
+ *   <Case is=valueN>
+ *     content if exp == valueN
+ *   </Case>
+ *   <Else>
+ *     content if no match (optional)
+ *   </Else>
  * </If>
  * ```
  */
@@ -71,45 +95,35 @@ class If_ extends Component
   /** @var IfProperties */
   public $props;
 
+  /**
+   * Evaluates the condition of the If clause.
+   *
+   * @return bool
+   */
   protected function evaluate ()
   {
     $prop = $this->props;
     $np   = Parser::NAMELESS_PROP;
+    $v    = isset ($prop->$np) ? $prop->$np : $prop->get ('value');
+    $not  = $prop->not;
 
-    if (isset($prop->$np)) {
-      if ($prop->$np)
-//      if ($prop->getComputed($np))
-        return $this->getChildren ();
-      return $this->getChildren ('else');
-    }
-
-    $v   = $prop->get ('value');
-    $not = $prop->not;
-
-    if (exists ($prop->matches)) {
-      if (preg_match ("%$prop->matches%", $v) xor $not)
-        return $this->getChildren ();
-      return $this->getChildren ('else');
-    }
+    if (exists ($prop->matches))
+      return (bool)preg_match ("%$prop->matches%", $v) xor $not;
 
     if ($prop->case) {
       foreach ($prop->case as $param) {
-        if ($v == $param->props->is)
-          return $param->getChildren ();
+        if ($v == $param->props->get ('is')) // Note that we use the loose comparison operator == here.
+          return true;
       }
-      return $this->getChildren ('else');
+      return false;
     }
 
-    if (toBool ($v) xor $not)
-      return $this->getChildren ();
-
-    return $this->getChildren ('else');
+    return strToBool ($v) xor $not;
   }
 
   protected function render ()
   {
-    $result = $this->evaluate ();
-    if ($result)
-      self::renderSet ($result);
+    self::renderSet ($this->getChildren ($this->evaluate () ? null : 'else'));
   }
+
 }
