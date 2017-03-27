67d6e5f1229b4fd60b5db0818e536ece6d47775c
Cláudio Silva <claudio.silva@impactwave.com>
2017-03-28 00:22:53 +0100

    FIX: RawText output.

--- src/Lib/JavascriptCodeGen.php
+++ src/Lib/JavascriptCodeGen.php
@@ -14,12 +14,12 @@ class JavascriptCodeGen
   {
     $o = mapAndFilter ($options, function ($v, $k) use ($indent) {
       if (is_object ($v)) {
+        if ($v instanceof \RawText)
+          return "$k: $v";
         if (method_exists ($v, 'toArray'))
           $v = $v->toArray ();
         else $v = (array)$v;
       }
-      if (is_object ($v) && $v instanceof \RawText)
-        return "$k: $v";
       return isset ($v)
         ? "$k: " . json_encode ($v, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
         : null;
