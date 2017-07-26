320a6992500100c3fb83890c0881d6b0d73461a9
Paulo Elias <paulo.elias@impactwave.com>
2017-07-26 16:20:12 +0100

    Melhoramento do componente FlashMessage

--- src/Components/FlashMessage.php
+++ src/Components/FlashMessage.php
@@ -2,42 +2,63 @@
 
 namespace Matisse\Components;
 
-use Electro\Exceptions\FlashType;
 use Electro\Interfaces\SessionInterface;
 use Matisse\Components\Base\HtmlComponent;
 use Matisse\Properties\Base\HtmlComponentProperties;
+
+class FlashMessageProperties extends HtmlComponentProperties
+{
+	public $errorClass = 'alert alert-danger';
+	public $infoClass = 'alert alert-info';
+	public $successClass = 'alert alert-success';
+	public $warningClass = 'alert alert-warning';
+}
 
 /**
  * Displays the flash message currently stored on the session, if any.
  */
 class FlashMessage extends HtmlComponent
 {
-  const propertiesClass = HtmlComponentProperties::class;
-  /**
-   * @var SessionInterface
-   */
-  private $session;
+	const TYPE_PROP_NAMES = [
+		'', 'errorClass', 'warningClass', 'infoClass', 'successClass',
+	];
+	const propertiesClass = FlashMessageProperties::class;
+	/**
+	 * @var SessionInterface
+	 */
+	private $session;
 
-  public function __construct (SessionInterface $session)
-  {
-    parent::__construct ();
-    $this->session = $session;
-  }
+	public function __construct (SessionInterface $session)
+	{
+		parent::__construct ();
+		$this->session = $session;
+	}
 
-  protected function isVisible ()
-  {
-    return exists ($this->session->getFlashMessage ()) && parent::isVisible ();
-  }
+	protected function isVisible ()
+	{
+		return exists ($this->session->getFlashMessage ()) && parent::isVisible ();
+	}
 
-  protected function render ()
-  {
-    if (!$this->props->class)
-      $this->addClass ('alert');
-    $msg   = $this->session->getFlashMessage ();
-    $class = FlashType::getLabel ($msg['type']);
-    $this->addClass ($class);
-    $this->beginContent ();
-    echo $msg['message'];
-  }
+	protected function postRender ()
+	{
+		parent::postRender();
+	}
+
+	protected function preRender ()
+	{
+		$msg       = $this->session->getFlashMessage ();
+		$classProp = self::TYPE_PROP_NAMES[$msg['type']];
+		if ($classProp)
+			$this->addClass ($this->props->$classProp);
+
+		parent::preRender();
+	}
+
+	protected function render ()
+	{
+		$this->beginContent();
+		$msg       = $this->session->getFlashMessage ();
+		echo $msg['message'];
+	}
 
 }
